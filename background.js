28705c545fbe6efdd08f05a1294eb4df698b9b39
Jaiom Rola <90425309+justjaiom@users.noreply.github.com>
2022-12-29 12:01:47 -0600

    Add files via upload

---
+++ background.js
@@ -0,0 +1,36 @@
+
+
+
+const adList = [
+    "*://*.doubleclick.net/*",
+    "*://*.googlesyndication.com/*",
+    "*://*.googletagservices.com/*",
+    "*://*.googleadservices.com/*",
+    "*://*.google-analytics.com/*",
+    "*://*.googleadservices.com/*",
+    "*://*.zedo.com/*",
+    "*://*.adbrite.com/*",
+    "*://*.adbureau.net/*",
+    "*://*.carbonads.net/*",
+    "*://*.cdn.carbonads.com/*",
+    "*://*.cdn.carbonads.net/*",
+    "*://*.cdn.doubleclick.net/*",
+    "*://*.cdn.googletagservices.com/*",
+    "*://*.cdn.googleadservices.com/*",
+    "*://*.cdn.zedo.com/*",
+];
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        return {cancel: true}
+    },
+    {urls: adList},
+    ["blocking"]
+)
+
+
+
+var pyautogui = require('pyautogui');
+var time = require('time');
+
+
